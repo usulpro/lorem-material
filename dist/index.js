c0c4fda874cba36c8d68079361438dabcfc7d9e2
UsulPro <regx@usul.su>
2016-12-26 21:10:13 +0700

    upd

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,16 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 var Lorem = 'Material';
 
 exports.default = Lorem;
+
+
+var lorems = require.context('./', true, /Material\w*.jsx/);
+lorems.keys().forEach(function (filename) {
+    var ipsum = lorems(filename).default;
+    var ipsumName = filename.replace(/^.\//, '').replace(/.jsx$/, '');
+    module.exports[ipsumName] = ipsum;
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 const Lorem = 'Material';
 
 export default Lorem;
+
+const lorems = require.context('./', true, /Material\w*.jsx/);
+lorems.keys().forEach((filename) => {
+    const ipsum = lorems(filename).default;
+    const ipsumName = filename.replace(/^.\//, '').replace(/.jsx$/, '');
+    module.exports[ipsumName] = ipsum;
+});
